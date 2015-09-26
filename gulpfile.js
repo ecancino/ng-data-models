989be87cd1a44338233c559149960d93e8eb822b
Eduardo Cancino <eduardo.cancino@ciudadde.mx>
2015-09-26 00:53:05 -0500

    Added browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,50 +1,31 @@
-var gulp = require('gulp'),
-  clean = require('del'),
-  jade = require('gulp-jade'),
-  browserify = require('gulp-browserify'),
-  uglify = require('gulp-uglify'),
-  jshint = require('gulp-jshint'),
-  sass = require('gulp-sass'),
-  minify = require('gulp-minify-css'),
-  rename = require('gulp-rename'),
-  livereload = require('gulp-livereload'),
-  tinylr = require('tiny-lr'),
-  express = require('express'),
-  connect = require('connect-livereload'),
-  lrs_port = 35729,
-  xps_port = 5000;
+var gulp      = require('gulp'),
+  clean       = require('del'),
+  jade        = require('gulp-jade'),
+  browserify  = require('gulp-browserify'),
+  uglify      = require('gulp-uglify'),
+  jshint      = require('gulp-jshint'),
+  sass        = require('gulp-sass'),
+  minify      = require('gulp-minify-css'),
+  rename      = require('gulp-rename'),
+  browser     = require('browser-sync').create();
 
-var livereload_server = tinylr(),
-  static_server = express();
-
-static_server.use(connect({
-  port: lrs_port
-}));
-static_server.use(
-  express.static('./public')
-);
-
-gulp.task('clean', function () {
-  clean([ './public/css' ]);
-  clean([ './public/js' ]);
+gulp.task('clean', function (cb) {
+  clean([ './public' ]);
   clean([ './tmp' ]);
+  cb();
 });
 
 gulp.task('jade', function () {
   return gulp.src('./src/index.jade')
-    .pipe(jade({
-      data: {
-        title: 'Gulp Boilerplate'
-      }
-    }))
+    .pipe(jade({ data: { title: 'Gulp Boilerplate' } }))
     .pipe(gulp.dest('./public'))
-    .pipe(livereload(livereload_server));
+    .pipe(browser.stream());
 });
 
 gulp.task('es6', function () {
-  return gulp.src('./src/app.jsx')
+  return gulp.src('./src/app.js')
     .pipe(browserify({
-      extensions: [ '.js', '.jsx' ],
+      extensions: [ '.js' ],
       transform: [ 'babelify' ],
       debug: true
     }))
@@ -53,7 +34,7 @@ gulp.task('es6', function () {
     .pipe(rename({suffix: '.min', extname: '.js'}))
     .pipe(uglify())
     .pipe(gulp.dest('./public/js'))
-    .pipe(livereload(livereload_server));
+    .pipe(browser.stream());
 });
 
 gulp.task('sass', function () {
@@ -63,7 +44,7 @@ gulp.task('sass', function () {
     .pipe(rename({suffix: '.min', extname: '.css'}))
     .pipe(minify({keepSpecialComments: 0}))
     .pipe(gulp.dest('./public/css'))
-    .pipe(livereload(livereload_server));
+    .pipe(browser.stream());
 });
 
 gulp.task('static', function () {
@@ -71,23 +52,26 @@ gulp.task('static', function () {
     .pipe(gulp.dest('./public/fonts'));
 });
 
-gulp.task('build', ['static', 'jade', 'es6', 'sass'], function(cb) {
+gulp.task('build', ['clean', 'static', 'jade', 'es6', 'sass'], function(cb) {
   cb();
 });
 
-gulp.task('watch', function (cb) {
-  livereload_server.listen();
+gulp.task('watch', ['build'], function (cb) {
   gulp.watch('./src/index.jade', ['jade']);
   gulp.watch([ './src/**/*.jsx' ], ['es6']);
   gulp.watch('./src/*.scss', ['sass']);
   cb();
 });
 
-gulp.task('serve', function () {
-  static_server.listen(xps_port);
-  livereload_server.listen(lrs_port);
+gulp.task('serve', ['watch'], function (cb) {
+  browser.init({
+    server: './public',
+    logFileChanges: false,
+    reloadOnRestart: true
+  });
+  cb();
 });
 
-gulp.task('default', ['clean', 'build', 'serve', 'watch'], function () {
+gulp.task('default', ['serve'], function () {
   console.log('Gulp and running!');
 });
