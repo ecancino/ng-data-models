1c692acc0b68cef366627e879e97bcb692776b5e
Eduardo Cancino <eduardo.cancino@ciudadde.mx>
2015-10-08 21:49:00 -0500

    Virtue is sufficient for happiness - Refactor list to work with Immutable.List

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,19 +1,24 @@
 import _ from 'lodash';
+import { List } from 'immutable';
 
 class Main {
     constructor(Post, Posts) {
-        this.posts = Posts;
+        this.posts = List(Posts);
         this.post = new Post();
         this.editing = false;
 
         const update = (post) => {
-            let updated = Post.update({ oid: post._id['$oid'] }, post);
-            let idx = _.findIndex(this.posts, post => post._id['$oid'] === updated._id['$oid']);
-            this.posts[idx] = updated;
+            let change = Post.update({ oid: post._id['$oid'] }, post);
+            let index = this.posts.findIndex(item => {
+                return item._id === change._id;
+            });
+            this.posts = this.posts.set(index, change);
         };
 
         const create = (post) => {
-            post.$save().then(saved => this.posts.push(saved));
+            post.$save().then((saved) => {
+                this.posts = this.posts.push(saved)
+            });
         };
 
         const save = (post) => {
@@ -25,11 +30,12 @@ class Main {
             reset();
         };
 
-        const remove = (post) => {
+        const remove = (index) => {
+            var post = this.posts.get(index);
             post.oid = post._id['$oid'];
             delete post._id;
             Post.delete(post);
-            _.remove(this.posts, post);
+            this.posts = this.posts.delete(index);
         };
 
         const reset = () => {
@@ -37,8 +43,8 @@ class Main {
             this.editing = false;
         };
 
-        const active = (post) => {
-            this.post = Post.get({ oid: post._id['$oid'] }, post);
+        const active = (index) => {
+            this.post = this.posts.get(index);
             this.editing = true;
         };
 
